2e2bdba0cffb2d2855495159a457cc712da3c156
cleeag <jack0506000@gmail.com>
2020-05-10 16:57:11 +0800

    initial commit

---
+++ infectionAnalyzer.cpp
@@ -0,0 +1,9 @@
+//this is one of the two files that you need to submit
+
+#include "infectionAnalyzer.h"
+#include <fstream> //for reading file
+#include <queue> //you may use STL queue
+#include <algorithm> //you may use STL algorithm
+using namespace std;
+//you are NOT allowed to include additional libraries yourself
+
---
+++ treeImplementation.h
@@ -0,0 +1,11 @@
+//this is one of the two files that you need to submit
+
+using namespace std;
+//you are NOT allowed to include additional libraries yourself
+
+//you do NOT need to include "tree.h" in this file
+//this is NOT a cpp file, but simply a part of "tree.h"
+//see the bottom of tree.h for explanation
+//just write your tree implementation here right away
+
+
